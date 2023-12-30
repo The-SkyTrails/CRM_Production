45b0aa8d58ef8a65f71a8427ad0145b63ce7339f
armaan <armaanalam65@gmail.com>
2023-12-30 11:13:43 +0530

    modify

--- SkyTrails/settings.py
+++ SkyTrails/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-t(@43l3j*w(li#6)2^qx^o54+z@21(ko^yd%^&3usuitd&x(a&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
